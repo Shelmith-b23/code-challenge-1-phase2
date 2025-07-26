f8a0554b6a8845b1b94dd455babf9a2da18fbbbe
Shelmith-b23 <shelwangui23@gmail.com>
2025-07-26 12:43:27 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import PlantPage from "./components/PlantPage";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <PlantPage />
+
+    </div>
+  );
+}
+
+export default App;
